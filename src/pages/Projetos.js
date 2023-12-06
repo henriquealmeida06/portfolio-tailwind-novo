769ba5fa45b1fe571b24ccba38963f61545b079f
Henrique Almeida <marceloh500@gmail.com>
2023-12-06 06:01:19 -0300

    ajustando projetos

--- src/pages/Projetos.js
+++ src/pages/Projetos.js
@@ -1,8 +1,7 @@
 import BlocoProjeto from "../components/BlocoProjeto";
-import Urna from "./../assets/img/urna.png";
 import Clima from "./../assets/img/clima.png";
-import Calculadora from "./../assets/img/calculadora.png";
-import Todo from "./../assets/img/img_todo.png";
+import Calculadora from "./../assets/img/calculadora_dark.png";
+import Todo from "./../assets/img/todo.png";
 import { FaHtml5, FaReact } from "react-icons/fa";
 import { IoLogoJavascript } from "react-icons/io5";
 import { SiStyledcomponents } from "react-icons/si";
@@ -27,15 +26,7 @@ function Projetos() {
       <h1 className="text-2xl font-bold text-center mb-24 md:mt-28">Projetos</h1>
 
       <div className="flex flex-wrap gap-11 items-center justify-center">
-        <BlocoProjeto
-          onClick={abrirUrna}
-          imagem={Urna}
-          nome="Urna (Em Construção)"
-          tecnologia1={<FaHtml5 />}
-          tecnologia2={<SiTailwindcss />}
-          tecnologia3={<IoLogoJavascript />}
-          tecnologia4={<FaReact />}
-        />
+       
         <BlocoProjeto
           onClick={abrirClima}
           imagem={Clima}
